6cbe36f7a853bd66a961c7cc6d5559e05ceba358
Phi Huynh <phihuynh@Phis-MacBook-Pro.local>
2020-08-05 02:03:22 -0600

    first commit

---
+++ build/precache-manifest.7d8ca463d71117f3973674358030de2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0ad21f1069a71ead779641cc39b393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3072456afd0ad33fc1d1",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "6bf5ef8c2a3483a2313f",
+    "url": "/static/js/2.cecb86d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cecb86d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3072456afd0ad33fc1d1",
+    "url": "/static/js/main.2e582792.chunk.js"
+  },
+  {
+    "revision": "39e6cfe83c757cf51d32",
+    "url": "/static/js/runtime-main.8248b067.js"
+  }
+]);
